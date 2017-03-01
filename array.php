37aeb4f9e136c40b9fb1ff9a8e5965438a0f3ca1
rodrigo-delgado <rod.delgado85@gmail.com>
2017-03-02 11:01:44 +1300

    push into array

---
+++ array.php
@@ -0,0 +1,16 @@
+<html>
+    <p>
+	<?php
+	// Create an array and push 5 elements on to it, then
+    // print the number of elements in your array to the screen
+    $arr = array();
+
+    array_push($arr, "H");
+    array_push($arr, "He");
+    array_push($arr, "HI");
+    array_push($arr, "Ho");
+    array_push($arr, "Hu");
+    print count($arr);
+	?>
+	</p>
+</html>
